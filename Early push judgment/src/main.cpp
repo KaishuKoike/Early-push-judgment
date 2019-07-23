6d7c23a77398a682f3e16f07dba5bc2a028ca16d
KaishuKoike <50565970+KaishuKoike@users.noreply.github.com>
2019-07-23 20:46:47 +0900

    tt

--- Early push judgment/src/main.cpp
+++ Early push judgment/src/main.cpp
@@ -1,127 +1,183 @@
-
+//Include
+//------------------------------------------------------------------//
 #include <M5Stack.h>
 #include <NeoPixelBus.h>
 
-const uint16_t PixelCount = 4; // this example assumes 4 pixels, making it smaller will cause a failure
-const uint8_t PixelPin = 2;  // make sure to set this to the correct pin, ignored for Esp8266
 
-#define colorSaturation 128
 
-// three element pixels, in different order and speeds
-NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount, PixelPin);
-//NeoPixelBus<NeoRgbFeature, Neo400KbpsMethod> strip(PixelCount, PixelPin);
+//Define
+//------------------------------------------------------------------//
+#define colorSaturation 255
+#define BUTTON1_PIN 21
+#define BUTTON2_PIN 22
+#define BUTTON3_PIN 23
+#define BUTTON4_PIN 19
 
-// For Esp8266, the Pin is omitted and it uses GPIO3 due to DMA hardware use.  
-// There are other Esp8266 alternative methods that provide more pin options, but also have
-// other side effects.
-// for details see wiki linked here https://github.com/Makuna/NeoPixelBus/wiki/ESP8266-NeoMethods 
+#define BUTTON_A_PIN 39
+#define BUTTON_B_PIN 38
+#define BUTTON_C_PIN 37
 
-// You can also use one of these for Esp8266, 
-// each having their own restrictions
-//
-// These two are the same as above as the DMA method is the default
-// NOTE: These will ignore the PIN and use GPI03 pin
-//NeoPixelBus<NeoGrbFeature, NeoEsp8266Dma800KbpsMethod> strip(PixelCount, PixelPin);
-//NeoPixelBus<NeoRgbFeature, NeoEsp8266Dma400KbpsMethod> strip(PixelCount, PixelPin);
+//Global
+//------------------------------------------------------------------//
 
-// Uart method is good for the Esp-01 or other pin restricted modules
-// for details see wiki linked here https://github.com/Makuna/NeoPixelBus/wiki/ESP8266-NeoMethods 
-// NOTE: These will ignore the PIN and use GPI02 pin
-//NeoPixelBus<NeoGrbFeature, NeoEsp8266Uart1800KbpsMethod> strip(PixelCount, PixelPin);
-//NeoPixelBus<NeoRgbFeature, NeoEsp8266Uart1400KbpsMethod> strip(PixelCount, PixelPin);
-
-// The bitbang method is really only good if you are not using WiFi features of the ESP
-// It works with all but pin 16
-//NeoPixelBus<NeoGrbFeature, NeoEsp8266BitBang800KbpsMethod> strip(PixelCount, PixelPin);
-//NeoPixelBus<NeoRgbFeature, NeoEsp8266BitBang400KbpsMethod> strip(PixelCount, PixelPin);
-
-// four element pixels, RGBW
-//NeoPixelBus<NeoRgbwFeature, Neo800KbpsMethod> strip(PixelCount, PixelPin);
 
 RgbColor red(colorSaturation, 0, 0);
-RgbColor green(0, colorSaturation, 0);
-RgbColor blue(0, 0, colorSaturation);
+RgbColor yellow(240, 255, 0);
+RgbColor green(0, 255, 0);
+RgbColor blue(0, 0, 255);
+RgbColor aqua(0, 204, 255);
 RgbColor white(colorSaturation);
+RgbColor pink(255,0,153);
 RgbColor black(0);
 
-HslColor hslRed(red);
-HslColor hslGreen(green);
-HslColor hslBlue(blue);
-HslColor hslWhite(white);
-HslColor hslBlack(black);
+
+const uint16_t PixelCount = 16; // this example assumes 4 pixels, making it smaller will cause a failure
+const uint8_t PixelPin = 18;  // make sure to set this to the correct pin, ignored for Esp8266
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount, PixelPin);
+
+boolean button1_status = 1;
+boolean button2_status = 1;
+boolean button3_status = 1;
+boolean button4_status = 1;
+
+boolean buttonA_status = 1;
+boolean buttonB_status = 1;
+boolean buttonC_status = 1;
+
+boolean ans_flag = 0;
+boolean TF_flag = 0;
+
+unsigned char pattern = 0;
+unsigned char active_team = 0;
 
 
+//Global
+//------------------------------------------------------------------//
+
+
+
+//Setup
+//------------------------------------------------------------------//
 void setup()
 {
-    Serial.begin(115200);
-    while (!Serial); // wait for serial attach
 
-    Serial.println();
-    Serial.println("Initializing...");
-    Serial.flush();
-
+    //M5.begin();
     // this resets all the neopixels to an off state
     strip.Begin();
     strip.Show();
 
+    pinMode(BUTTON1_PIN, INPUT_PULLUP);
+    pinMode(BUTTON2_PIN, INPUT_PULLUP);
+    pinMode(BUTTON3_PIN, INPUT_PULLUP);
+    pinMode(BUTTON4_PIN, INPUT_PULLUP);
 
-    Serial.println();
-    Serial.println("Running...");
+    pinMode(BUTTON_A_PIN, INPUT);
+    pinMode(BUTTON_B_PIN, INPUT);
+    pinMode(BUTTON_C_PIN, INPUT);
+
 }
 
+//Main
+//------------------------------------------------------------------//
 
 void loop()
 {
-    delay(5000);
 
-    Serial.println("Colors R, G, B, W...");
+    switch ( pattern ) {
+    case 0:
+        button1_status = digitalRead( BUTTON1_PIN );
+        button2_status = digitalRead( BUTTON2_PIN );
+        button3_status = digitalRead( BUTTON3_PIN );
+        button4_status = digitalRead( BUTTON4_PIN );
+        if ( button1_status == 0 ) {
+            pattern = 11;
+        } else if ( button2_status == 0 ) {
+            pattern = 21;
+        } else if ( button3_status == 0 ) {
+            pattern = 31;
+        } else if ( button4_status == 0 ) {
+            pattern = 41;
+        }
+        break;
 
-    // set the colors, 
-    // if they don't match in order, you need to use NeoGrbFeature feature
-    strip.SetPixelColor(0, red);
-    strip.SetPixelColor(1, green);
-    strip.SetPixelColor(2, blue);
-    strip.SetPixelColor(3, white);
-    // the following line demonstrates rgbw color support
-    // if the NeoPixels are rgbw types the following line will compile
-    // if the NeoPixels are anything else, the following line will give an error
-    //strip.SetPixelColor(3, RgbwColor(colorSaturation));
-    strip.Show();
+    case 11:
+        strip.ClearTo( pink, 0, 3);
+        strip.Show();
+        active_team = 1;
+        pattern = 51;
+        break;
+
+    case 21:
+        strip.ClearTo( aqua, 4, 7);
+        strip.Show();
+        active_team = 2;
+        pattern = 51;
+        break;
+
+    case 31:
+        strip.ClearTo( green, 8, 11);
+        strip.Show();
+        active_team = 3;
+        pattern = 51;
+        break;
+
+    case 41:
+        strip.ClearTo( yellow, 12, 15);
+        strip.Show();
+        active_team = 4;
+        pattern = 51;
+        break;
+
+    case 51:
+        buttonA_status = digitalRead( BUTTON_A_PIN );
+        buttonB_status = digitalRead( BUTTON_B_PIN );
+        buttonC_status = digitalRead( BUTTON_C_PIN );
+        if ( buttonA_status == 0 ) {
+            TF_flag = 1;
+            pattern = 61;
+            break;
+        } else if ( buttonB_status == 0 ) {
+            TF_flag = 0;
+            pattern = 61;
+            break;
+        } else if ( buttonC_status == 0 ) {
+            pattern = 71;
+        }
+        break;
+
+    case 61:
+        if( TF_flag ) {
+            for(int i=0; i<=5; i++) {
+                strip.ClearTo( blue, 4 * active_team - 4, 4 * active_team - 1);
+                strip.Show();
+                delay(100);
+                strip.ClearTo( 0, 4 * active_team - 4, 4 * active_team - 1);
+                strip.Show();
+                delay(100);
+            }
+        } else {
+            for(int i=0; i<=5; i++) {
+                strip.ClearTo( red, 4 * active_team - 4, 4 * active_team - 1);
+                strip.Show();
+                delay(100);
+                strip.ClearTo( 0, 4 * active_team - 4, 4 * active_team - 1);
+                strip.Show();
+                delay(100);
+            }
+        }
+        pattern = 71;
+        break;
+
+    case 71:
+        strip.ClearTo( 0, 0, 15);
+        strip.Show();
+        TF_flag = 0;
+        active_team = 0;
+        pattern = 0;
+        break;
+
+    }
 
 
-    delay(5000);
-
-    Serial.println("Off ...");
-
-    // turn off the pixels
-    strip.SetPixelColor(0, black);
-    strip.SetPixelColor(1, black);
-    strip.SetPixelColor(2, black);
-    strip.SetPixelColor(3, black);
-    strip.Show();
-
-    delay(5000);
-
-    Serial.println("HSL Colors R, G, B, W...");
-
-    // set the colors, 
-    // if they don't match in order, you may need to use NeoGrbFeature feature
-    strip.SetPixelColor(0, hslRed);
-    strip.SetPixelColor(1, hslGreen);
-    strip.SetPixelColor(2, hslBlue);
-    strip.SetPixelColor(3, hslWhite);
-    strip.Show();
-
-
-    delay(5000);
-
-    Serial.println("Off again...");
-
-    // turn off the pixels
-    strip.SetPixelColor(0, hslBlack);
-    strip.SetPixelColor(1, hslBlack);
-    strip.SetPixelColor(2, hslBlack);
-    strip.SetPixelColor(3, hslBlack);
-    strip.Show();
 
 }
